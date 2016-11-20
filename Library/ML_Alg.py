630118c1127d35d11a8b09bd6f2cd6b6f1715f70
Christopher Mertin <cmertin@cs.utah.edu>
2016-11-20 14:06:58 -0700

    Implemented generalized linear and logistic regression

--- Library/ML_Alg.py
+++ Library/ML_Alg.py
@@ -4,36 +4,62 @@ from __future__ import print_function, division
 import numpy as np
 import numpy.linalg as npl
 import scipy.spatial.distance as spd
+import copy as copy
 from math import sqrt
-'''
-# y is the numebr of data points
-def J_derivative(theta, x, y, h):
-    temp = 0
-    for i in range(0, len(y)):
-        temp = temp + (h(x[i], theta) - y[i]) * x[i])
-    return temp/len(y)
-# Computes the gradient descent to minimize the function and get the best fitting
-# values for theta_0 and theta_1. It continually loops until the frobenius norm
-# of the previous values of theta minus the new values of theta is less than some
-# tolerance (defaults to 1e-6).
-# alpha is the learning rate
-# This function stores each value of theta over each iteration so that the
-# evolution of theta can be plotted. It returns the final value as "theta" and
-# it returns a list of values as theta_data
-def GradientDescent(theta, x, y, alpha, tol=1e-6, h=h_linear):
+
+# Hypothesis function for linear regression
+def h_linear(x, theta):
+    return np.inner(x, theta)
+
+# Hypothesis function for logistic regression
+def h_logistic_regression(x, theta):
+    exponent = np.inner(x, theta)
+    denominator = 1 + np.exp(-exponent)
+    return 1/denominator
+
+# Calcualtes the d/dt of the cost function (J) given a hypothesis
+# function. Used for Gradient Descent. It returns a vector of d/dt
+# for each feature
+def J_Derivative(theta, x, y, h):
+    d_dt = []
+    for feature in range(0, x.shape[1]):
+        temp = 0
+        for data in range(0, x.shape[0]):
+            temp = temp + (h(x[data], theta) - y[data]) * x[data][feature]
+        d_dt.append(temp / x.shape[0])
+    return d_dt
+
+# Method of Gradient Descent, to find the minimum for the classification function.
+# theta: Inital guess for the parameters
+# x: Data points size: [N,M] (N data points, M features per data)
+# y: The "true/labeled" values for the given data
+# alpha: The learning rate for the function
+# lambda_: Used for regularization. Using lambda = 0 ignores regularization
+# tol: The tolerance/stopping point for the vector
+# updates: If true, it returns a vector theta_updates which contains the values
+#          of theta for each iteration so you can see how it evolves
+def GradientDescent(theta, x, y, alpha, h, lambda_=0, tol=1e-6, updates=False):
     theta_old = copy.deepcopy(theta) + 10
-        while npl.norm(theta - theta_old) > tol:
-            theta_data.append(theta.copy())
-            theta_old = theta.copy()
-            d_dt = J_Derivative(theta, x, y)
-            theta[0] = theta[0] - alpha * d_dt[0]
-            theta[1] = theta[1] - alpha * d_dt[1]
-    return theta, theta_data
+    theta_updates = []
+    while npl.norm(theta - theta_old) > tol:
+        if updates == True:
+            theta_updates.append(theta.copy())
+        theta_old = theta.copy()
+        d_dt = J_Derivative(theta, x, y, h)
+        for i in range(0, theta.shape[0]):
+            theta[i] = theta[i] * (1 - alpha * lambda_ / len(x)) - alpha * d_dt[i]
+    if updates == True:
+        return theta, theta_updates
+    else:
+        return theta
 
+# Performs Linear Regression on given data: See GradientDescent for arguments
+def LinearRegression(theta, x, y, alpha, lambda_=0, tol=1e-6, updates=False):
+    return GradientDescent(theta, x, y, alpha, h_linear, lambda_, tol, updates)
 
-def Linear_Regression(x, y, alpha, tol=1e-6, h=h_linear):
-    theta = np.zeros(len(y))
-'''
+# Performs Logistic Regression on given data: See GradientDescent for arguments
+def LogisticRegression(theta, x, y, alpha, lambda_=0, tol=1e-6, updates=False):
+    return GradientDescent(theta, x, y, alpha, h_logistic_regression, lambda_, tol, updates)
 
 # The values are simply theta^T * x
 # x: Feature vector
@@ -55,6 +81,16 @@ def NormalEquation(X, y):
     temp = np.dot(temp, XT) # (X^T * X)^-1 * X^T
     return np.dot(temp, y)  # theta = (X^T * X)^-1 * X^T * y
 
+# Normal Equation with Regularization
+def NormalEquation_Reg(X, y, lambda_):
+    XT = np.transpose(X)    # X^T
+    l_mat = lambda_ * np.eye(XT.shape[0]) # lambda along the diagonal
+    l_mat[0][0] = 0         # Except the first term
+    temp = np.dot(XT, X)    # X^T * X
+    temp = temp + l_mat     # X^T * X + lambda * I
+    temp = npl.pinv(temp)   # Calcualtes the pseudo-inverse to deal with singularity
+    temp = np.dot(temp, XT) # (X^T * X + lambda * I)^-1 * X^T
+    return np.dot(temp, y)  # theta = (X^T * X + lambda * I)^-1 * X^T * y
 
 # Performs K-Nearest Neighbors on the given data and returns the k neighbors
 # c: data to perform KNN on
