0ed51e6f37a2f49a2fd01d4152499434c6db6d07
Christopher Mertin <cmertin@cs.utah.edu>
2016-11-20 20:48:18 -0700

    Added SVM

--- Credit_Card_Fraud/credit_fraud.py
+++ Credit_Card_Fraud/credit_fraud.py
@@ -7,6 +7,9 @@ import numpy as np
 import seaborn as sns
 sns.set(color_codes=True)
 import pandas as pd
+from sklearn.svm import LinearSVC
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import classification_report
 from scipy import stats, integrate
 # Gets the path/library with my Machine Learning Programs and adds it to the
 # current PATH so it can be imported
@@ -17,8 +20,23 @@ from ML_Alg import LogisticRegression, UpSample
 from f_io import ReadCSV
 
 data_file = "creditcard.csv"
-FILE = os.getcwd() + "/data/" + data_file
+DIR = os.getcwd() + "/data/"
+FILE = DIR + data_file
 
 x, y = ReadCSV(FILE)
 
-UpSample(x, y)
+data, test_data = UpSample(x, y)
+
+print("Finished Up Sampling Data")
+
+svm = LinearSVC()
+print("Running Linear SVM")
+svm.fit(data[0], data[1])
+y_pred = svm.predict(test_data[0])
+print(classification_report(y_pred, test_data[1]))
+
+log_reg = LogisticRegression()
+print("Running Logistic Regression")
+log_reg.fit(data[0], data[1])
+y_pred = log_reg.predict(test_data[0])
+print(classification_report(y_pred, test_data[1]))
